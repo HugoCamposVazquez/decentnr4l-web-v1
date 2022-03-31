4cf959ac2a2a7e1d4e8f2edee0e22751b77419ae
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-03-31 13:50:51 +0200

    heroku

---
+++ FRONTEND/src/components/About-header/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+
+const AboutHeader = () => {
+  return (
+    <section className="inicio">
+      <div className="container-fluid inicio">
+        <div className="row">
+          <div className="col-lg-12">
+          <span className="izquierda">BLOCK</span>
+          <span className="derecha">CHAIN</span>
+          {/* <h1 className="inicio-texto"><span className="uno">SERVICIOS</span><span className="dos">BLOKCHAIN</span></h1> */}
+            <h1 className="inicio-texto"><span className="uno">SERVICIOS</span>
+            <span className="dos">BLOK</span><span className="tres">CHAIN</span></h1>
+          </div>
+        </div>
+      </div>
+      <div className="container segundo">  
+          <div className="row">
+            <div className="col-lg-12">
+              <div className="cont text-center">
+                <div className="path">
+                <Link href="#contact"><a>Contáctanos</a></Link>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>  
+
+    </section>
+  );
+};
+
+export default AboutHeader;
