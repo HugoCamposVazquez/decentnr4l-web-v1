50abee94292b5d2f944620745efa6c6c02125a4a
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-03-31 22:28:59 +0200

    heroku

--- src/pages/project-details2/project-details2-dark.jsx
+++ src/pages/project-details2/project-details2-dark.jsx
@@ -10,27 +10,10 @@ import NextProject from "../../components/Next-project";
 import SmallFooter from "../../components/Small-footer";
 
 const ProjectDetails2Dark = () => {
-    const navbarRef = React.useRef(null);
-    const logoRef = React.useRef(null);
-    React.useEffect(() => {
-      var navbar = navbarRef.current,
-        logo = logoRef.current;
-      if (window.pageYOffset > 300) {
-        navbar.classList.add("nav-scroll");
-      } else {
-        navbar.classList.remove("nav-scroll");
-      }
-      window.addEventListener("scroll", () => {
-        if (window.pageYOffset > 300) {
-          navbar.classList.add("nav-scroll");
-        } else {
-          navbar.classList.remove("nav-scroll");
-        }
-      });
-    }, [navbarRef]);
+
     return (
       <DarkTheme>
-        <Navbar nr={navbarRef} lr={logoRef} />
+       
         <ProjectDetails2Header />
         <ProjectDetails2Introduction />
         <ProjectDetails2Images />
