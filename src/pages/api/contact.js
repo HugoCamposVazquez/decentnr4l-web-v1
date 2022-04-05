9229d2743a30793c1fb9426b4de3954c456f5e41
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-05 20:01:49 +0200

    pre-strapi

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,5 +1,6 @@
-export default (req, res) => {
-    const body = JSON.parse(req.body);
-    console.log(body);
-    res.status(200).json({ status: 'OK' });
-  };
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+export default function handler(req, res) {
+  res.status(200).json({ status: 'Ok' })
+}
