39c4ff4f79e72aa227b1ac96bfed5621a3de2bfc
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-05 22:23:33 +0200

    form added

--- src/components/Contact-section/index.jsx
+++ src/components/Contact-section/index.jsx
@@ -1,65 +1,150 @@
-import { react, useState } from 'react';
-import Split from "../Split";
-
-
+import { useForm } from 'react-hook-form';
+import axios from 'axios';
+import { useRouter } from 'next/router';
+import { useToast } from '../../hooks/useToast';
 
 export default function ContactSection() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
-
-  const handleSubmit = e => {
-    fetch('/api/contact', {
+  const toast = useToast();
+  const { register, handleSubmit, errors, reset } = useForm();
+  const router = useRouter();
+  async function onSubmitForm(values) {
+    let config = {
       method: 'post',
-      body: JSON.stringify(data),
-    });
-    e.preventDefault();
-    const data = {
-      name,
-      email,
-      message,
+      url: `/api/contact`,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      data: values,
     };
-    console.log(data);
-  };
+
+    try {
+      const response = await axios(config);
+      console.log(response);
+      if (response.status == 200) {
+        reset();
+        toast(
+          'success',
+          'Thank you for contacting us, we will be in touch soon.'
+        );
+      }
+    } catch (err) {}
+  }
 
   return (
-    <section className="contact-sec">
-      <div className="container-fluid">
-        <div className="sec-head custom-font text-center">
-          <h6>
-            Get In Touch
-          </h6>
-          <span className="tbg">Contact</span>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-lg-10">
-          <form onSubmit={handleSubmit}>
-          <label htmlFor="name">Name:</label>
-          <input
-            id="name"
-            type="text"
-            onChange={e => setName(e.target.value)}
-          />
-          <label htmlFor="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            onChange={e => setEmail(e.target.value)}
-          />
-          <label htmlFor="message">Message:</label>
-          <textarea
-            id="message"
-            type="text"
-            rows="4"
-            onChange={e => setMessage(e.target.value)}
-          />
-          <button type="submit">Send</button>
-        </form>
+    <div className="b py-16 bg-gray-50 px-4 sm:px-6 h-screen w-screen flex justify-center items-center">
+      <div className="mx-auto w-full max-w-2xl rounded-xl bg-white p-8 shadow">
+        <form
+          onSubmit={handleSubmit(onSubmitForm)}
+          className="grid grid-cols-1 gap-y-6">
+           <div className='row'>
+          <div>
+            <label htmlFor="name" className="sr-only">
+              Full name
+            </label>
+            <input
+              type="text"
+              name="name"
+              ref={register({
+                required: {
+                  value: true,
+                  message: 'You must enter your name',
+                },
+              })}
+              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
+                errors.name ? 'ring-2 ring-red-500' : null
+              }`}
+              placeholder="Full name"
+            />
+            <span className="text-red-400 text-sm py-2">
+              {errors?.name?.message}
+            </span>
           </div>
-        </div>
+          <div>
+            <label htmlFor="email" className="sr-only">
+              Email
+            </label>
+            <input
+              name="email"
+              type="text"
+              ref={register({
+                required: {
+                  value: true,
+                  message: 'You must enter your email address',
+                },
+                minLength: {
+                  value: 8,
+                  message: 'This is not long enough to be an email',
+                },
+                maxLength: {
+                  value: 120,
+                  message: 'This is too long',
+                },
+                pattern: {
+                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                  message: 'This needs to be a valid email address',
+                },
+              })}
+              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
+                errors.email ? 'ring-2 ring-red-500' : null
+              }`}
+              placeholder="Email"
+            />
+            <span className="text-red-400 text-sm py-2">
+              {errors?.email?.message}
+            </span>
+          </div>
+          <div>
+            <label htmlFor="phone" className="sr-only">
+              Phone
+            </label>
+            <input
+              type="text"
+              name="phone"
+              ref={register}
+              className="block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2"
+              placeholder="Telegram"
+            />
+          </div>
+          </div> 
+          <div className='mensaje'>
+            <label htmlFor="message" className="sr-only">
+              Message
+            </label>
+            <textarea
+              name="message"
+              rows="4"
+              ref={register({
+                required: {
+                  value: true,
+                  message: 'You need to enter your message',
+                },
+                maxLength: {
+                  value: 1000,
+                  message: "Your message can't be more than 1000 characters",
+                },
+                minLength: {
+                  value: 50,
+                  message: 'Your message must be longer than this!',
+                },
+              })}
+              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
+                errors.message ? 'ring-2 ring-red-500' : null
+              }`}
+              placeholder="Message"></textarea>
+            <span className="text-red-400 text-sm py-2">
+              {errors?.message?.message}
+            </span>
+          </div>
+          <p>he leido y acepto blah blahblah</p>
+          <div>
+            <button
+              type="submit"
+              className="inline-flex justify-center py-3 px-6 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
-    </section>
+    </div>
   );
-};
-
-
+}
--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -1,6 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ status: 'Ok' })
-}
+import nodemailer from 'nodemailer';
+require("dotenv").config();
+export default async (req, res) => {
+
+  const { name, email, message, phone } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.user,
+      pass: process.env.pass,
+    },
+  });
+
+  try {
+    const emailRes = await transporter.sendMail({
+    from: email,
+    to: 'decentr4l@gmail.com',
+    subject: `Mensaje desde la web Decentr4l ${name}`,
+    html: `<p>Tienes un nuevo mensaje</p><br>
+    <p><strong>Nombre: </strong> ${name} </p><br>
+    <p><strong>telegram: </strong> ${phone} </p><br>
+    <p><strong>Mensaje: </strong> ${message} </p><br>
+
+    `,
+    });
+
+    console.log('Message Sent');
+  } catch (err) {
+    console.log(err);
+  }
+
+  res.status(200).json(req.body);
+};
