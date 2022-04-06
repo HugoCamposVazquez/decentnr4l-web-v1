5701c71e194bbb86d98345469101e6466d7a84fd
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-06 12:26:54 +0200

    contact form with toast

--- src/components/Contact-section/index.jsx
+++ src/components/Contact-section/index.jsx
@@ -1,10 +1,9 @@
 import { useForm } from 'react-hook-form';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-import { useToast } from '../../hooks/useToast';
+import toast, { Toaster } from "react-hot-toast";
 
 export default function ContactSection() {
-  const toast = useToast();
   const { register, handleSubmit, errors, reset } = useForm();
   const router = useRouter();
   async function onSubmitForm(values) {
@@ -18,9 +17,9 @@ export default function ContactSection() {
     };
 
     try {
-      const response = await axios(config);
-      console.log(response);
-      if (response.status == 200) {
+      const res = await axios(config);
+      console.log(res);
+      if (res.status === 200) {
         reset();
         toast(
           'success',
@@ -31,81 +30,77 @@ export default function ContactSection() {
   }
 
   return (
+    <><div>sdsd</div><div>dfdf</div>
+    
     <div className="b py-16 bg-gray-50 px-4 sm:px-6 h-screen w-screen flex justify-center items-center">
       <div className="mx-auto w-full max-w-2xl rounded-xl bg-white p-8 shadow">
+        <span className="izquierda">BLOCK</span>
         <form
           onSubmit={handleSubmit(onSubmitForm)}
           className="grid grid-cols-1 gap-y-6">
-           <div className='row'>
-          <div>
-            <label htmlFor="name" className="sr-only">
-              Full name
-            </label>
-            <input
-              type="text"
-              name="name"
-              ref={register({
-                required: {
-                  value: true,
-                  message: 'You must enter your name',
-                },
-              })}
-              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.name ? 'ring-2 ring-red-500' : null
-              }`}
-              placeholder="Full name"
-            />
-            <span className="text-red-400 text-sm py-2">
-              {errors?.name?.message}
-            </span>
+          <div className='row'>
+            <div>
+              <label htmlFor="name" className="sr-only">
+                Full name
+              </label>
+              <input
+                type="text"
+                name="name"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: 'You must enter your name',
+                  },
+                })}
+                className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${errors.name ? 'ring-2 ring-red-500' : null}`}
+                placeholder="Full name" />
+              <span className="text-red-400 text-sm py-2">
+                {errors?.name?.message}
+              </span>
+            </div>
+            <div>
+              <label htmlFor="email" className="sr-only">
+                Email
+              </label>
+              <input
+                name="email"
+                type="text"
+                ref={register({
+                  required: {
+                    value: true,
+                    message: 'You must enter your email address',
+                  },
+                  minLength: {
+                    value: 8,
+                    message: 'This is not long enough to be an email',
+                  },
+                  maxLength: {
+                    value: 120,
+                    message: 'This is too long',
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'This needs to be a valid email address',
+                  },
+                })}
+                className={`emailclass block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${errors.email ? 'ring-2 ring-red-500' : null}`}
+                placeholder="Email" />
+              <span className="text-red-400 text-sm py-2">
+                {errors?.email?.message}
+              </span>
+            </div>
+            <div>
+              <label htmlFor="phone" className="sr-only">
+                Phone
+              </label>
+              <input
+                type="text"
+                name="phone"
+                ref={register}
+                className="block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2"
+                placeholder="Telegram" />
+            </div>
           </div>
-          <div>
-            <label htmlFor="email" className="sr-only">
-              Email
-            </label>
-            <input
-              name="email"
-              type="text"
-              ref={register({
-                required: {
-                  value: true,
-                  message: 'You must enter your email address',
-                },
-                minLength: {
-                  value: 8,
-                  message: 'This is not long enough to be an email',
-                },
-                maxLength: {
-                  value: 120,
-                  message: 'This is too long',
-                },
-                pattern: {
-                  value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-                  message: 'This needs to be a valid email address',
-                },
-              })}
-              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.email ? 'ring-2 ring-red-500' : null
-              }`}
-              placeholder="Email"
-            />
-            <span className="text-red-400 text-sm py-2">
-              {errors?.email?.message}
-            </span>
-          </div>
-          <div>
-            <label htmlFor="phone" className="sr-only">
-              Phone
-            </label>
-            <input
-              type="text"
-              name="phone"
-              ref={register}
-              className="block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2"
-              placeholder="Telegram"
-            />
-          </div>
-          </div> 
           <div className='mensaje'>
             <label htmlFor="message" className="sr-only">
               Message
@@ -123,28 +118,27 @@ export default function ContactSection() {
                   message: "Your message can't be more than 1000 characters",
                 },
                 minLength: {
-                  value: 50,
+                  value: 20,
                   message: 'Your message must be longer than this!',
                 },
               })}
-              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${
-                errors.message ? 'ring-2 ring-red-500' : null
-              }`}
-              placeholder="Message"></textarea>
+              className={`block w-full shadow py-3 px-4 placeholder-gray-500 focus:ring-blue-500 focus:border-blue-500 border-gray-300 rounded-md focus:outline-none focus:ring-2 ${errors.message ? 'ring-2 ring-red-500' : null}`}
+              placeholder="Cuéntanos lo que necesites"></textarea>
             <span className="text-red-400 text-sm py-2">
               {errors?.message?.message}
             </span>
           </div>
-          <p>he leido y acepto blah blahblah</p>
+          <p className='aceptar'>Enviando el mensaje acepto la política de privacidad que todavía no está creada.</p>
           <div>
             <button
               type="submit"
               className="inline-flex justify-center py-3 px-6 border border-transparent shadow text-base font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-              Submit
+              Enviar mensaje
             </button>
           </div>
+          <Toaster />
         </form>
       </div>
-    </div>
+    </div></>
   );
 }
