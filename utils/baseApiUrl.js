d1a53f68db5a2dbc65ba124801b21cd61041aefd
Víctor Hugo Campos Vázquez <vcamposvazquez@gmail.com>
2022-04-14 14:25:23 +0200

    blog added with strapi

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -1,5 +1,5 @@
 const baseApiUrl = process.env.NODE_ENV === "production" 
-? 'http://localhost:1337' 
+? 'http://https://back4d.herokuapp.com' 
 : 'http://localhost:1337';
 
 export default baseApiUrl;
